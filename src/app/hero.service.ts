4ccdc2701664e35657af182d18cc21eab7366393
Bryan <bryan_stevens314@yahoo.com>
2019-07-21 16:21:11 -0700

    Feature: Delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,5 +55,13 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === `number` ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id= ${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 
 }
